cf10564ae7f4a88f65b418f8eb1eb4c8961dba11
pufferffish <me@windtfw.com>
2024-12-14 19:07:22 +0000

    strip unnecessary libraries

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 
-extern crate clap;
-extern crate env_logger;
-
 extern crate winapi;
 extern crate kernel32;
-extern crate serde;
-extern crate serde_json;
 
 #[macro_use]
 extern crate log;
